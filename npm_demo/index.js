ffffdf5132eb6e03d1b6ddba7cac2fffda7bfb31
beginner-g <969925523@qq.com>
2017-08-30 22:47:37 +0800

    package

---
+++ npm_demo/index.js
@@ -0,0 +1,4 @@
+// var $ =require('./node_modules/jquery')
+// console.log($)
+var num =require("./index1.js")
+console.log(num(1,3))
---
+++ npm_demo/index1.js
@@ -0,0 +1,4 @@
+function hello(x,y){
+  return x+y
+}
+module.exports=hello
